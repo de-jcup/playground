61d51798c48cb926163fce840b9f6e9bdbe89f15
Albert Tregnaghi <de-jcup@users.noreply.github.com>
2018-03-09 12:04:26 +0100

    Create error_example.go

---
+++ go/error_example.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+/* see https://tour.golang.org/methods/19 */
+
+type MyError struct {
+	When time.Time
+	What string
+}
+
+func (e *MyError) Error() string {
+	return fmt.Sprintf("Ups. at %v, %s",
+		e.When, e.What)
+}
+
+func run() error {
+	return &MyError{
+		time.Now(),
+		"it didn't work",
+	}
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Println(err)
+	}
+}
