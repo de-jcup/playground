e3216062808f56f5073a7b7068d90fbd7fcca46f
Albert Tregnaghi <de-jcup@users.noreply.github.com>
2018-03-09 10:44:40 +0100

    Update map_example_main.go

--- go/map_example_main.go
+++ go/map_example_main.go
@@ -9,12 +9,13 @@ type Vertex struct {
 var m map[string]Vertex
 
 func main() {
+	fmt.Println("-- vertex map")
 	m = make(map[string]Vertex)
 	m["Bell Labs"] = Vertex{
 		40.68433, -74.39967,
 	}
 	fmt.Println(m["Bell Labs"])
-
+	fmt.Println("-- string map")
 	m2 := make(map[string]string)
 	m2["key-a"] = "value-a"
 	m2["key-b"] = "value-b"
@@ -23,4 +24,12 @@ func main() {
 	fmt.Printf("key-a=%s\n", m2["key-a"])
 	fmt.Printf("key-b=%s\n", m2["key-b"])
 	fmt.Printf("key-not-existing=%s\n", m2["key-not-existing"])
+	
+	
+	fmt.Println("-- vertex map with literals")
+	m3:= map[string]Vertex{
+		"Bell Labs": {40.68433, -74.39967},
+		"Google":    {37.42202, -122.08408},
+	}
+	fmt.Println(m3)
 }
